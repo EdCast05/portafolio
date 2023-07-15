c17d312619c4ebd07fa848c8971f58c2bd9c1879
EdCast05 <eduarcastanedam@gmail.com>
2023-07-14 23:03:44 -0500

    Creating components, and work in about me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    NavBarComponent,
+    AboutmeComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
